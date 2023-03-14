622e45888fe256b1c268f5b1f972e02675a2ffa9
Joseph Abatan05 <103934571+JosephAbatan05@users.noreply.github.com>
2023-03-14 08:11:27 -0700

    Create main.js

---
+++ .github/workflows/main.js
@@ -0,0 +1,17 @@
+// link to javascript file //
+function increment(){
+    console.log("Welcome to  interactive spaces")
+}
+increment()
+//console.log("welcome to interactive spaces")
+
+
+function add(){
+    console.log("Successfully purchased")
+}
+add()
+function commit(){
+    console.log("Terminate purchases")
+}
+commit()
+
